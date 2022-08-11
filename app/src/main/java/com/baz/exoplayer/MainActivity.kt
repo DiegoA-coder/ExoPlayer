b8d4580f4b676256d63deac333afab49dd15e471
DiegoA-coder <ogeid12@outlook.es>
2022-08-11 12:36:11 -0500

    new-version

--- app/src/main/java/com/baz/exoplayer/MainActivity.kt
+++ app/src/main/java/com/baz/exoplayer/MainActivity.kt
@@ -2,133 +2,57 @@ package com.baz.exoplayer
 
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.MediaItem.AdsConfiguration
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.ext.ima.ImaAdsLoader
-import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
-import com.google.android.exoplayer2.source.MediaSourceFactory
+import com.google.android.exoplayer2.source.ads.AdsMediaSource
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.util.Util
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 
 class MainActivity : AppCompatActivity() {
 
-    private var playerView: PlayerView? = null
-    private var player: ExoPlayer? = null
-    private var adsLoader: ImaAdsLoader? = null
-    private var skipAds: Button? = null
+    //Player
+    private var exoplayer: SimpleExoPlayer? = null
+
+    //private val imaAdsLoader: ImaAdsLoader? = null
+    var url: String? = ""
+
+
+    //HLS
+    //val contentUri = Uri.parse("https://ice55.securenetsystems.net/DASH27.m3u")
+    val contentUri =
+        Uri.parse("https://mdstrm.com/live-stream-playlist/60c9357b09f72f0830c30058.m3u8")
+
+    //Ads
+    val adTagUri =
+        "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator="
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playerView = findViewById<PlayerView>(R.id.player_view)
-        skipAds = findViewById(R.id.buttonSkipAds)
+        var player2 = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
+        var playerView = findViewById<PlayerView>(R.id.player_view)
+        playerView.player = player2
+        //val imaAdsLoader = ImaAdsLoader(this, Uri.parse(adTagUri))
 
-        // Create an AdsLoader.
-        adsLoader = ImaAdsLoader.Builder( /* context= */this).build()
+        val dataSourceFactory = DefaultDataSourceFactory(this, "ExoPlayer")
+        val hlsMediaSource =
+            HlsMediaSource.Factory(dataSourceFactory).createMediaSource(contentUri)
+        val dashMediaSource =
+            DashMediaSource.Factory(dataSourceFactory).createMediaSource(contentUri)
 
-        //Skip Ads
-        skipAds!!.setOnClickListener(View.OnClickListener {
-            try {
-                onDestroy()
-            } catch (e: Exception) {
-                println(e)
-            }
-        })
-    }
+        //val adsMediaSource = AdsMediaSource(hlsMediaSource, dataSourceFactory, imaAdsLoader, playerView)
 
-    private fun releasePlayer() {
-        adsLoader!!.setPlayer(null)
-        playerView!!.player = null
-        player!!.release()
-        player = null
-    }
+        player2?.prepare(hlsMediaSource)
+        player2?.setPlayWhenReady(true)
 
-    override fun onStart() {
-        super.onStart()
-        //
-        if (Util.SDK_INT > 23) {
-            initializePlayer()
-            if (playerView != null) {
-                playerView!!.onResume()
-            }
-        }
-    }
 
-    override fun onResume() {
-        super.onResume()
-        if (Util.SDK_INT <= 23 || player == null) {
-            initializePlayer()
-            if (playerView != null) {
-                playerView!!.onResume()
-            }
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (Util.SDK_INT <= 23) {
-            if (playerView != null) {
-                playerView!!.onPause()
-            }
-            releasePlayer()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        if (Util.SDK_INT > 23) {
-            if (playerView != null) {
-                playerView!!.onPause()
-            }
-            releasePlayer()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        adsLoader!!.release()
-    }
-
-    private fun initializePlayer() {
-        val contentUri = Uri.parse(getString(R.string.content_url))
-        val adTagUri = Uri.parse(getString(R.string.ad_tag_url))
-
-        // Create a SimpleExoPlayer and set it as the player for content and ads.
-        player = ExoPlayer.Builder(this)
-            .setMediaSourceFactory(buildMediaSource())
-            .build()
-        playerView!!.player = player
-        adsLoader!!.setPlayer(player)
-
-        val mediaItem = MediaItem.Builder()
-            .setUri(contentUri)
-            .setAdsConfiguration(
-                AdsConfiguration.Builder(adTagUri)
-                    .setAdsId(adTagUri)
-                    .build()
-            )
-            .build()
-
-        // Prepare the content and ad to be played with the SimpleExoPlayer.
-        player!!.setMediaItem(mediaItem)
-        player!!.prepare()
-
-        // Set PlayWhenReady. If true, content and ads will autoplay.
-        player!!.setPlayWhenReady(false)
-    }
-
-    //creating mediaSource
-    private fun buildMediaSource(): MediaSourceFactory {
-        val mediaSourceFactory: MediaSourceFactory =
-            DefaultMediaSourceFactory(this)
-                .setAdsLoaderProvider { unusedAdTagUri: AdsConfiguration? -> adsLoader }
-                .setAdViewProvider(playerView)
-
-        return mediaSourceFactory
     }
 }
